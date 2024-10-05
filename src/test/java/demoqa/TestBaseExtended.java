d1adad1c2511ae6fb8620f8985edf053bf75bfe2
sbrownbear <sergey.gsarh@gmail.com>
2024-10-05 20:17:01 +0300

    added helpers attachments - screenshots and logs

--- src/test/java/demoqa/TestBaseExtended.java
+++ src/test/java/demoqa/TestBaseExtended.java
@@ -2,7 +2,9 @@ package demoqa;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -28,8 +30,16 @@ public class TestBaseExtended {
     }
 
     @BeforeEach
-    void beforeEach() {
+    void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last Screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+
     }
 
 }
