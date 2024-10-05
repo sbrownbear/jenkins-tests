c381a860a98d87744015a0e97aee5216ebd93fa3
sbrownbear <sergey.gsarh@gmail.com>
2024-10-05 18:38:32 +0300

    added allure listener and steps

--- src/test/java/demoqa/TestBaseExtended.java
+++ src/test/java/demoqa/TestBaseExtended.java
@@ -1,7 +1,10 @@
 package demoqa;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -22,6 +25,11 @@ public class TestBaseExtended {
                 "enabledVNC", true,
                 "enabledVideo", true
         ));
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
 }
